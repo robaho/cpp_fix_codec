31638a3bac970a6592126e05039d70cc1aff83b4
robert engels <robaho@users.noreply.github.com>
2025-01-15 10:53:47 -0600

    use union for FieldOrGroup to reduce size

--- fieldmap.cpp
+++ fieldmap.cpp
@@ -18,12 +18,12 @@ const Group Group::EMPTY = Group();
 FieldMap& FieldMap::addGroup(uint32_t tag) {
     auto itr = map.find(tag);
     if(itr==nullptr || !itr->isGroup()) {
-        auto& grp = map.put({Field::EMPTY,Group(tag, 1),tag}).group;
+        auto& grp = map.put(Group(tag, 1)).group();
         FieldMap *obj = (FieldMap*)buffer.allocate(sizeof(FieldMap));
         grp.groups->push_back(new (obj) FieldMap(buffer));
         return *grp.groups->at(0);
     }
-    auto& grp = itr->group;
+    auto& grp = itr->group();
     // FieldMap *obj = (FieldMap*)buffer.allocate(sizeof(FieldMap));
     grp.groups->push_back(new FieldMap(buffer));
     return *grp.groups->back();
@@ -31,23 +31,23 @@ FieldMap& FieldMap::addGroup(uint32_t tag) {
 FieldMap& FieldMap::addGroup(uint32_t tag,int count) {
     auto itr = map.find(tag);
     if(itr==nullptr || !itr->isGroup()) {
-        auto& grp = map.put({Field::EMPTY,Group(tag,count),tag}).group;
+        auto& grp = map.put(Group(tag,count)).group();
         FieldMap *obj = (FieldMap*)buffer.allocate(sizeof(FieldMap));
         grp.groups->push_back(new (obj) FieldMap(buffer));
         return *grp.groups->at(0);
     }
-    auto& grp = itr->group;
+    auto& grp = itr->group();
     FieldMap *obj = (FieldMap*)buffer.allocate(sizeof(FieldMap));
     grp.groups->push_back(new (obj) FieldMap(buffer));
     return *grp.groups->back();
 }
-void FieldMap::set(uint32_t tag, Field field){
-    map.put({field,Group::EMPTY,tag});
+void FieldMap::set(uint32_t tag, const Field& field){
+    map.put(field);
 }
-Field FieldMap::get(uint32_t tag) const {
-    auto tmp = map.find(tag);
-    if(tmp!=nullptr && tmp->isField()) {
-        return tmp->field;
+const Field& FieldMap::get(uint32_t tag) const {
+    auto itr = map.find(tag);
+    if(itr!=nullptr && itr->isField()) {
+        return itr->field();
     } else {
         return Field::EMPTY;
     }
@@ -59,23 +59,23 @@ const FieldMap& FieldMap::getGroup(uint32_t tag,int index) const {
         oss << "tag " << tag << " is not a group";
         throw std::runtime_error(oss.str());
     }
-    return *itr->group.groups->at(index);
+    return *itr->group().groups->at(index);
 }
 int FieldMap::getGroupCount(uint32_t tag) const {
-    auto tmp = map.find(tag);
-    if(tmp==nullptr || !tmp->isGroup()) {
+    auto itr = map.find(tag);
+    if(itr==nullptr || !itr->isGroup()) {
         std::ostringstream oss;
         oss << "tag " << tag << " is not a group";
         throw std::runtime_error(oss.str());
     }
-    return tmp->group.count;
+    return itr->group().count;
 }
 int FieldMap::getGroupSize(uint32_t tag) const {
-    auto tmp = map.find(tag);
-    if(tmp==nullptr || !tmp->isGroup()) {
+    auto itr = map.find(tag);
+    if(itr==nullptr || !itr->isGroup()) {
         std::ostringstream oss;
         oss << "tag " << tag << " is not a group";
         throw std::runtime_error(oss.str());
     }
-    return tmp->group.groups->size();
+    return itr->group().groups->size();
 }
--- fieldmap.h
+++ fieldmap.h
@@ -42,57 +42,90 @@ struct Group {
 };
 
 struct FieldOrGroup {
-    Field field;
-    Group group;
+private:
+    bool _group = false;
+    union FieldType {
+        FieldType() {}
+        Field field;
+        Group group;
+    } fg;
+public:
     uint32_t tag;
     bool isGroup() {
-        return group.tag==tag;
+        return _group;
     }
     bool isField() {
-        return field.tag==tag;
+        return !_group;
+    }
+    const Group& group() {
+        return fg.group;
+    }
+    const Field& field() {
+        return fg.field;
+    }
+    FieldOrGroup(const Group& group) : _group(true), tag(group.tag) {
+        new(&fg.group) Group(group);
+    }
+    FieldOrGroup(const Field& field) : _group(false), tag(field.tag) {
+        new(&fg.group) Field(field);
+    }
+    FieldOrGroup(const FieldOrGroup& other) : _group(other._group), tag(other.tag) {
+        if (_group) {
+            new(&fg.group) Group(other.fg.group);
+        } else {
+            new(&fg.field) Field(other.fg.field);
+        }
     }
 };
 
 class FieldList {
-private:
+    // TODO: possibly add hash map for quicker indexed lookup of fields, but the
+    // structure is so small that the linear search is often best
+  private:
     using VecAllocatorType = FieldMapAllocator<FieldOrGroup>;
 
-    std::vector<FieldOrGroup,VecAllocatorType> list;
-public:
-    FieldList(FieldMapBuffer& buffer): list(VecAllocatorType(buffer)) {
+    std::vector<FieldOrGroup, VecAllocatorType> list;
+
+  public:
+    FieldList(FieldMapBuffer &buffer) : list(VecAllocatorType(buffer)) {
         list.reserve(16);
     }
-    FieldOrGroup& put(const FieldOrGroup& fg) {
-        for(auto itr=list.begin();itr!=list.end();itr++) {
-            if(itr->tag==fg.tag) {
-                *itr=std::move(fg);
-                return *itr;
+
+    // add or replace a field/group in the list
+    FieldOrGroup &put(const FieldOrGroup &fg) {
+        for (auto itr = list.begin(); itr != list.end(); itr++) {
+            if (itr->tag == fg.tag) {
+              *itr = std::move(fg);
+              return *itr;
             }
         }
         list.push_back(std::move(fg));
         return list.back();
     }
 
-    FieldOrGroup& get(const uint32_t tag) {
-        for(auto itr=list.begin();itr!=list.end();itr++) {
-            if(itr->tag==tag) {
-                return *itr;
+    // get a field/group in the list by tag, or throw exception if it does not exist
+    FieldOrGroup &get(const uint32_t tag) {
+        for (auto itr = list.begin(); itr != list.end(); itr++) {
+            if (itr->tag == tag) {
+              return *itr;
             }
         }
         throw std::runtime_error("tag does not exist");
     }
+    // determine if a field/group is in the list
     bool contains(const uint32_t tag) const {
-        for(auto itr=list.begin();itr!=list.end();itr++) {
-            if(itr->tag==tag) {
-                return true;
+        for (auto itr = list.begin(); itr != list.end(); itr++) {
+            if (itr->tag == tag) {
+              return true;
             }
         }
         return false;
     }
-    FieldOrGroup* find(const uint32_t tag) const {
-        for(auto itr=list.begin();itr!=list.end();itr++) {
-            if(itr->tag==tag) {
-                return const_cast<FieldOrGroup*>(&(*itr));
+    // find a field/group is in the list
+    FieldOrGroup *find(const uint32_t tag) const {
+        for (auto itr = list.begin(); itr != list.end(); itr++) {
+            if (itr->tag == tag) {
+              return const_cast<FieldOrGroup *>(&(*itr));
             }
         }
         return nullptr;
@@ -120,8 +153,8 @@ public:
      * @return a mutable FieldMap to which fields and groups can be added
      */
     FieldMap& addGroup(uint32_t tag,int count);
-    void set(uint32_t tag, Field field);
-    Field get(uint32_t tag) const;
+    void set(uint32_t tag, const Field& field);
+    const Field& get(uint32_t tag) const;
     const FieldMap& getGroup(uint32_t tag,int index) const;
     // returns the number of groups expected 
     int getGroupCount(uint32_t tag) const;
