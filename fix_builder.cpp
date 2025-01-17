1b34f49522522ae0e85b0985d4b32f7dfc2ea97f
robert engels <robaho@users.noreply.github.com>
2025-01-17 17:29:25 -0600

    remove duplicate definition

--- fix_builder.cpp
+++ fix_builder.cpp
@@ -1,10 +1,3 @@
 #include "fix_builder.h"
 #include <stdexcept>
 #include <unistd.h>
-
-void FixBuilder::writeTo(int fd) {
-    int toWrite = cp-message;
-    int length = write(fd,message,toWrite);
-    if(length!=toWrite) throw std::runtime_error("failed to write to fd");
-    reset();
-}
